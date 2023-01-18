d89d4e1288cadd74c441ecba05a735279857867e
Alex Geis <siegxela@gmail.com>
2023-01-17 20:28:17 -0700

    complete Util.MakeCSV functionality and test output is correct

--- CatWorx.BadgeMaker/Util.cs
+++ CatWorx.BadgeMaker/Util.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Collections.Generic;
 
 namespace CatWorx.BadgeMaker
@@ -24,7 +25,35 @@ namespace CatWorx.BadgeMaker
 
         public static void MakeCSV(List<Employee> employees)
         {
-            // code here
+            // Check to see if folder exists
+            if (!Directory.Exists("data"))
+            {
+                // If not, create it
+                Directory.CreateDirectory("data");
+            }
+
+            // the keyword "using" has two distinct meanings that depend on the context:
+            // 1.  importing a namespace
+            // 2. temporarily using a resource)
+            // remember that whatever is defined in the parentheses is ONLY available within the subsequent set of curly braces. Once that block of code runs, the resource (in this case, StreamWriter) is removed.
+            using (StreamWriter file = new StreamWriter("data/employees.csv"))
+            {
+                file.WriteLine("ID,Name,PhotoUrl");
+                // loop over employees
+                for (int i = 0; i < employees.Count; i++)
+                {
+                    // 2nd arg = padding, e.g. -10 = left-aligned 10 space pad
+                    string template = "{0,-10}\t{1,-20}\t{2}";
+                    file.WriteLine(
+                        String.Format(
+                            template,
+                            employees[i].GetId(),
+                            employees[i].GetFullName(),
+                            employees[i].GetPhotoUrl()
+                        )
+                    );
+                }
+            }
         }
     }
 }
