1a63e2dffbb857bc1bc8d38c7c08b08940ca5c45
Alex Geis <siegxela@gmail.com>
2023-01-18 10:12:58 -0700

    verify API call is returning status 200 and install/import Newtonsoft.Json package

---
+++ CatWorx.BadgeMaker/PeopleFetcher.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace CatWorx.BadgeMaker
+{
+    class PeopleFetcher
+    {
+        public static List<Employee> GetEmployees()
+        {
+            List<Employee> employees = new List<Employee>();
+            // Collect user values until the value is an empty string
+            while (true)
+            {
+                Console.WriteLine("Please enter a name: (leave empty to exit): ");
+
+                string firstName = Console.ReadLine() ?? "";
+                // Break if the user hits ENTER without typing a name
+                if (firstName == "")
+                {
+                    break;
+                }
+
+                // add a Console.ReadLine() for each value
+                Console.Write("Enter last name: ");
+                string lastName = Console.ReadLine() ?? "";
+                Console.Write("Enter ID: ");
+                int id = Int32.Parse(Console.ReadLine() ?? "");
+                Console.Write("Enter Photo URL:");
+                string photoUrl = Console.ReadLine() ?? "";
+                Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
+                employees.Add(currentEmployee);
+            }
+            return employees;
+        }
+
+        async public static Task<List<Employee>> GetFromApi()
+        {
+            List<Employee> employees = new List<Employee>();
+
+            using (HttpClient client = new HttpClient())
+            {
+                string response = await client.GetStringAsync(
+                    "https://randomuser.me/api/?results=10&nat=us&inc=name,id,picture"
+                );
+                Console.WriteLine(response);
+            }
+
+            return employees;
+        }
+    }
+}
--- CatWorx.BadgeMaker/Program.cs
+++ CatWorx.BadgeMaker/Program.cs
@@ -9,37 +9,10 @@ namespace CatWorx.BadgeMaker
 {
     class Program
     {
-        static List<Employee> GetEmployees()
-        {
-            List<Employee> employees = new List<Employee>();
-            // Collect user values until the value is an empty string
-            while (true)
-            {
-                Console.WriteLine("Please enter a name: (leave empty to exit): ");
-
-                string firstName = Console.ReadLine() ?? "";
-                // Break if the user hits ENTER without typing a name
-                if (firstName == "")
-                {
-                    break;
-                }
-
-                // add a Console.ReadLine() for each value
-                Console.Write("Enter last name: ");
-                string lastName = Console.ReadLine() ?? "";
-                Console.Write("Enter ID: ");
-                int id = Int32.Parse(Console.ReadLine() ?? "");
-                Console.Write("Enter Photo URL:");
-                string photoUrl = Console.ReadLine() ?? "";
-                Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
-                employees.Add(currentEmployee);
-            }
-            return employees;
-        }
-
         async static Task Main(string[] args)
         {
-            List<Employee> employees = GetEmployees();
+            // List<Employee> employees = PeopleFetcher.GetEmployees();
+            List<Employee> employees = await PeopleFetcher.GetFromApi();
             Util.PrintEmployees(employees);
             Util.MakeCSV(employees);
             await Util.MakeBadges(employees);
