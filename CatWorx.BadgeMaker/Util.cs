18c51143ba269cfd9dd085b85c5975d8f5087acf
Alex Geis <siegxela@gmail.com>
2023-01-18 09:35:32 -0700

    update MakeBadge function logic to draw employee url image onto bitmap badge canvas

--- CatWorx.BadgeMaker/Util.cs
+++ CatWorx.BadgeMaker/Util.cs
@@ -61,6 +61,15 @@ namespace CatWorx.BadgeMaker
 
         async public static Task MakeBadges(List<Employee> employees)
         {
+            // Layout variables
+            int BADGE_WIDTH = 669;
+            int BADGE_HEIGHT = 1044;
+
+            int PHOTO_LEFT_X = 184;
+            int PHOTO_TOP_Y = 215;
+            int PHOTO_RIGHT_X = 486;
+            int PHOTO_BOTTOM_Y = 517;
+
             using (HttpClient client = new HttpClient())
             {
                 for (int i = 0; i < employees.Count; i++)
@@ -68,8 +77,19 @@ namespace CatWorx.BadgeMaker
                     SKImage photo = SKImage.FromEncodedData(
                         await client.GetStreamAsync(employees[i].GetPhotoUrl())
                     );
+                    SKImage background = SKImage.FromEncodedData(File.OpenRead("badge.png"));
 
-                    SKData data = photo.Encode();
+                    SKBitmap badge = new SKBitmap(BADGE_WIDTH, BADGE_HEIGHT);
+                    SKCanvas canvas = new SKCanvas(badge);
+
+                    canvas.DrawImage(background, new SKRect(0, 0, BADGE_WIDTH, BADGE_HEIGHT));
+                    canvas.DrawImage(
+                        photo,
+                        new SKRect(PHOTO_LEFT_X, PHOTO_TOP_Y, PHOTO_RIGHT_X, PHOTO_BOTTOM_Y)
+                    );
+
+                    SKImage finalImage = SKImage.FromBitmap(badge);
+                    SKData data = finalImage.Encode();
                     data.SaveTo(File.OpenWrite("data/employeeBadge.png"));
                 }
             }
