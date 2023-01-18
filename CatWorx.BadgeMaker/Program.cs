c8ec0a27b9376a533c6bab5d14af7384fca508ce
Alex Geis <siegxela@gmail.com>
2023-01-17 20:14:37 -0700

    in Program.cs, replace print employee function with new util.function dot notation

--- CatWorx.BadgeMaker/Program.cs
+++ CatWorx.BadgeMaker/Program.cs
@@ -37,27 +37,10 @@ namespace CatWorx.BadgeMaker
             return employees;
         }
 
-        static void PrintEmployees(List<Employee> employees)
-        {
-            for (int i = 0; i < employees.Count; i++)
-            {
-                // 2nd arg = padding, e.g. -10 = left-aligned 10 space pad
-                string template = "{0,-10}\t{1,-20}\t{2}";
-                Console.WriteLine(
-                    String.Format(
-                        template,
-                        employees[i].GetId(),
-                        employees[i].GetFullName(),
-                        employees[i].GetPhotoUrl()
-                    )
-                );
-            }
-        }
-
         static void Main(string[] args)
         {
             List<Employee> employees = GetEmployees();
-            PrintEmployees(employees);
+            Util.PrintEmployees(employees);
         }
     }
 }
